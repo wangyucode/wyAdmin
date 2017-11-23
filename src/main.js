e6f64150b719cc6df5b8986a1774b0ace15d457b
wayne <www_wayne_001@163.com>
2017-11-23 14:05:52 +0800

    因为无法打包font awesome 对font awesome使用cdn加速

--- src/main.js
+++ src/main.js
@@ -11,7 +11,6 @@ Vue.use(BootstrapVue)
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import 'font-awesome/css/font-awesome.min.css'
 
 Vue.config.productionTip = false
 
