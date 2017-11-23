e4cd87f7f5180d761405cf0a529313270d0dd94e
wayne <www_wayne_001@163.com>
2017-11-23 10:38:08 +0800

    登录

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,6 @@
     /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
     /*-webkit-font-smoothing: antialiased;*/
     /*-moz-osx-font-smoothing: grayscale;*/
-    text-align: center;
     /*width:100%;*/
     /*height:100%;*/
     /*position: absolute;*/
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,8 +1,25 @@
 <template>
-    <div class="container center-block">
-      <h1>this is Login</h1>
+  <div class="container">
+    <div class="row">
+      <div class="tech-stack col-6">
+        <h2>技术栈</h2>
+        <ul>
+          <li>1234</li>
+          <li>1234</li>
+          <li>1234</li>
+          <li>1234</li>
+        </ul>
+      </div>
+      <form class="form-signin col-4 offset-2">
+        <h2 class="form-signin-heading">登录</h2>
+        <p class="text-info">可以使用guest guest 游客登录</p>
+        <input type="text" id="inputUser" class="form-control" placeholder="用户名" required="" autofocus="">
+        <input type="password" id="inputPassword" class="form-control" placeholder="密码" required="">
+        <button class="btn btn-primary btn-block" type="submit">登录</button>
+        <a class = "register" href="#">没有账号？立即注册！</a>
+      </form>
     </div>
-
+  </div>
 </template>
 
 
@@ -14,4 +31,29 @@
 
 
 <style scoped>
+
+  .form-signin-heading{
+    text-align: center;
+    margin-bottom: 12px;
+  }
+
+  .register{
+    margin-top: 12px;
+    font-size: 12px;
+  }
+
+  .row {
+    margin-top: 32px;
+  }
+
+  input,button{
+    margin-top: 6px;
+  }
+
+  .form-signin {
+    background-color: #eef;
+    padding: 16px;
+    border: 1px solid #c2cfd6;
+    border-radius: 4px;
+  }
 </style>
