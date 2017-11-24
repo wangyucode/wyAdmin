28dc78ef2eafa0ec14a3e0e6e4223df08aece04c
wayne <www_wayne_001@163.com>
2017-11-24 15:14:11 +0800

    更新标题，更新icon

--- src/components/NavWycode.vue
+++ src/components/NavWycode.vue
@@ -1,29 +1,29 @@
 <template>
   <nav class="navbar navbar-dark navbar-expand-lg" style="background-color: rgba(0,140,160,0.6)">
     <div class="container">
-      <a class="navbar-brand" href="http://wycode.cn" target="_self"><img src="../../static/img/logo_48.png" width="32" height="32" alt="wycode.cn"> 王郁的小站</a>
+      <a class="navbar-brand" href="https://wycode.cn" target="_self"><img src="https://wycode.cn/img/logo_48.png" width="32" height="32" alt="wycode.cn"> 王郁的小站</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" href="http://wycode.cn/blog/"><i class="fa fa-pencil fa-lg" aria-hidden="true"></i> 博客</a>
+            <a class="nav-link" href="https://wycode.cn/blog/"><i class="fa fa-pencil fa-lg" aria-hidden="true"></i> 博客</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="http://wycode.cn/lab.html"><i class="fa fa-flask fa-lg" aria-hidden="true"></i> 实验室</a>
+            <a class="nav-link" href="https://wycode.cn/web/page/lab"><i class="fa fa-flask fa-lg" aria-hidden="true"></i> 实验室</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="http://wycode.cn/web/doc/"><i class="fa fa-plug fa-lg" aria-hidden="true"></i> API</a>
+            <a class="nav-link" href="https://wycode.cn/web/"><i class="fa fa-plug fa-lg" aria-hidden="true"></i> API</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link active" href="http://wycode.cn/vue/"><i class="fa fa-cog fa-lg" aria-hidden="true"></i> 后台</a>
+            <a class="nav-link active" href="https://wycode.cn/vue/"><i class="fa fa-cog fa-lg" aria-hidden="true"></i> 后台</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="http://wycode.cn/android-docs/reference/packages.html"><i class="fa fa-android fa-lg" aria-hidden="true"></i> 安卓文档</a>
+            <a class="nav-link" href="https://wycode.cn/android-docs/reference/packages.html"><i class="fa fa-android fa-lg" aria-hidden="true"></i> 安卓文档</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="http://wycode.cn/about.html"><i class="fa fa-user fa-lg" aria-hidden="true"></i> 关于</a>
+            <a class="nav-link" href="https://wycode.cn/about.html"><i class="fa fa-user fa-lg" aria-hidden="true"></i> 关于</a>
           </li>
         </ul>
       </div>
